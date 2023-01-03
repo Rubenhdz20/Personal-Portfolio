b578deea2e00f7d614abcde3764c0036cdff5185
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2023-01-02 23:13:20 -0700

    mobile menu working

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelector('.header-mobile__menu').addEventListener('click', animateBars);
+
+let header = document.querySelector('.header');
+let bar1 = document.querySelector('.bars-menu-1');
+let bar2 = document.querySelector('.bars-menu-2');
+let bar3 = document.querySelector('.bars-menu-3');
+let nav =  document.querySelector('#nav');
+let link =  document.querySelector('.navigation-link');
+
+function animateBars() {
+    bar1.classList.toggle('activebars-menu-1');
+    bar2.classList.toggle('activebars-menu-2');
+    bar3.classList.toggle('activebars-menu-3');
+    nav.classList.toggle('active-navigation-nav');
+}
+
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      bar1.classList.toggle('activebars-menu-1');
+      bar2.classList.toggle('activebars-menu-2');
+      bar3.classList.toggle('activebars-menu-3');
+      nav.classList.toggle('active-navigation-nav');
+    }
+
+})});
